252786c7188a064799246637600796600a2700ac
3rdVoyager <palebluedot316@gmail.com>
2026-09-10 09:33:20 -0400

    Refactor header and footer import structure in JS and HTML files, enhance CSS styles for navigation.

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,24 @@
-const header = document.querySelector('header');
-const footer = document.querySelector('footer');
-
-header.innerHTML = `
-    <nav>
-        <a href="index.html">
-            <img src="/assets/favicon/web-app-manifest-512x512.png" class="brand-logo"alt="Pilgrimage Homeschool Logo" />
-            <span>Pilgrimage Homeschool</span>
-        </a>
-        <ul>
-            <li><a href="index.html" class="nav-link">Home</a></li>
-            <li><a href="/about/index.html" class="nav-link">About</a></li>
-            <li><a href="/contact/index.html" class="nav-link">Contact</a></li>
-        </ul>
-    </nav>
+const header = `
+    <header class="site-header">
+        <nav>
+            <a href="index.html">
+                <img src="/assets/favicon/web-app-manifest-512x512.png" class="brand-logo"alt="Pilgrimage Homeschool Logo" />
+                <span>Pilgrimage Homeschool</span>
+            </a>
+            <ul>
+                <li><a href="index.html" class="nav-link">Home</a></li>
+                <li><a href="/about/index.html" class="nav-link">About</a></li>
+                <li><a href="/contact/index.html" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
 `;
 
-footer.innerHTML = `
-    <p>&copy; Pilgrimage Homeschool 2026. All rights reserved.</p>
+const footer = `
+    <footer class="site-footer">
+        <p>&copy; Pilgrimage Homeschool 2026. All rights reserved.</p>
+    </footer>
 `;
+
+document.body.insertAdjacentHTML("afterbegin", header);
+document.body.insertAdjacentHTML("beforeend", footer);
